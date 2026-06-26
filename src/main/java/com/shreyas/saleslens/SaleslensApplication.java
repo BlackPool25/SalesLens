78226f462d0b38907a38b47f3cf9c396655584db
Shreyas Joshi <shreyasjoshi2511@gmail.com>
2026-06-26 21:27:27 +0530

    chore: add springdoc, @EnableCaching, @EnableMethodSecurity for Phase 9

--- src/main/java/com/shreyas/saleslens/SaleslensApplication.java
+++ src/main/java/com/shreyas/saleslens/SaleslensApplication.java
@@ -2,6 +2,7 @@ package com.shreyas.saleslens;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableAsync
 @EnableKafka
+@EnableCaching
 public class SaleslensApplication {
 
     public static void main(String[] args) {
